158b26693338bb85e925a1509c74557d206d082b
AnnaPravdiukova <annagekata82@gmail.com>
2023-10-20 17:04:05 +0300

    added nodemon, added try-catch and error handling

--- backend/useless-app/src/index.js
+++ backend/useless-app/src/index.js
@@ -8,147 +8,20 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
-// app.post(`/signup`, async (req, res) => {
-//   const { name, email, posts } = req.body
-
-//   const postData = posts
-//     ? posts.map((post) => {
-//         return { title: post.title, content: post.content || undefined }
-//       })
-//     : []
-
-//   const result = await prisma.user.create({
-//     data: {
-//       name,
-//       email,
-//       posts: {
-//         create: postData,
-//       },
-//     },
-//   })
-//   res.json(result)
-// })
-
-// app.post(`/post`, async (req, res) => {
-//   const { title, content, authorEmail } = req.body
-//   const result = await prisma.post.create({
-//     data: {
-//       title,
-//       content,
-//       author: { connect: { email: authorEmail } },
-//     },
-//   })
-//   res.json(result)
-// })
-
-// app.put('/post/:id/views', async (req, res) => {
-//   const { id } = req.params
-
-//   try {
-//     const post = await prisma.post.update({
-//       where: { id: Number(id) },
-//       data: {
-//         viewCount: {
-//           increment: 1,
-//         },
-//       },
-//     })
-
-//     res.json(post)
-//   } catch (error) {
-//     res.json({ error: `Post with ID ${id} does not exist in the database` })
-//   }
-// })
-
-// app.put('/publish/:id', async (req, res) => {
-//   const { id } = req.params
-
-//   try {
-//     const postData = await prisma.post.findUnique({
-//       where: { id: Number(id) },
-//       select: {
-//         published: true,
-//       },
-//     })
-
-//     const updatedPost = await prisma.post.update({
-//       where: { id: Number(id) || undefined },
-//       data: { published: !postData.published || undefined },
-//     })
-//     res.json(updatedPost)
-//   } catch (error) {
-//     res.json({ error: `Post with ID ${id} does not exist in the database` })
-//   }
-// })
-
-// app.delete(`/post/:id`, async (req, res) => {
-//   const { id } = req.params
-//   const post = await prisma.post.delete({
-//     where: {
-//       id: Number(id),
-//     },
-//   })
-//   res.json(post)
-// })
-
-// app.get('/products', async (req, res) => {
-//   const products = await prisma.product.findMany()
-//   res.json(products)
-// })
-
-// app.get('/user/:id/drafts', async (req, res) => {
-//   const { id } = req.params
-
-//   const drafts = await prisma.user
-//     .findUnique({
-//       where: {
-//         id: Number(id),
-//       },
-//     })
-//     .posts({
-//       where: { published: false },
-//     })
-
-//   res.json(drafts)
-// })
-
-app.get(`/product/:id`, async (req, res) => {
+app.get(`/products/:id`, async (req, res) => {
   const { id } = req.params
 
-  const product = await prisma.product.findUnique({
-    where: { id: Number(id) },
-  })
-  res.json(product)
+  try {
+    const product = await prisma.product.findUnique({
+      where: { id: Number(id) },
+    })
+    res.json(product)
+  } catch (error) {
+    console.log('/products/:id [error]', error.message)
+    res.status(404).send('Not found.')
+  }
+
 })
-
-// app.get('/feed', async (req, res) => {
-//   const { searchString, skip, take, orderBy } = req.query
-
-//   const or = searchString
-//     ? {
-//         OR: [
-//           { title: { contains: searchString } },
-//           { content: { contains: searchString } },
-//         ],
-//       }
-//     : {}
-
-//   const posts = await prisma.post.findMany({
-//     where: {
-//       published: true,
-//       ...or,
-//     },
-//     include: { author: true },
-//     take: Number(take) || undefined,
-//     skip: Number(skip) || undefined,
-//     orderBy: {
-//       updatedAt: orderBy || undefined,
-//     },
-//   })
-
-//   res.json(posts)
-// })
 
 app.get('/products', async (req, res) => {
   const { searchString, skip, take, orderBy } = req.query
@@ -157,26 +30,29 @@ app.get('/products', async (req, res) => {
     ? {
         OR: [
           { name: { contains: searchString } },
-          { rate: { equals: Number(searchString) } },
-          { price: { equals: Number(searchString) } },
+          { rate: { equals: Number?.(searchString) || undefined } },
+          { price: { equals: Number?.(searchString) || undefined } },
           { description: { contains: searchString } },
         ],
       }
     : {};
-  console.log('or', or)
-
-  const products = await prisma.product.findMany({
-    where: {
-      ...or,
-    },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
-    orderBy: {
-      updatedAt: orderBy || undefined,
-    },
-  })
-
-  res.json(products)
+    try {
+      const products = await prisma.product.findMany({
+        where: {
+          ...or,
+        },
+        take: Number(take) || undefined,
+        skip: Number(skip) || undefined,
+        orderBy: {
+          name: orderBy || undefined,
+        },
+      })
+    
+      return res.json(products)
+    } catch (error) {
+      console.log(error)
+      return res.json([])
+    }
 })
 
 const server = app.listen(8081, () =>
